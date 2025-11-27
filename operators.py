37036418e5ee078e5669da689bd2fe6ddccf75e9
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2025-11-27 20:51:25 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,29 @@
+# operators
+
+# arithmetic operators
+# + - * / %
+
+num1 = 10
+num2 = 5    
+
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+# print(num1 % num2)
+
+# assignment operators
+# += -= *= /= %= 
+num1 += 5
+num1 -= 5
+# print(num1)
+
+# conditional operators
+# > < >= <= == !=
+print(num1 > num2)
+
+# and or not
+
+print(num1 > num2 and num2 == 10)
+print(num1 > num2 or num2 == 10)
+print(not(num1 > num2 or num2 == 10))
---
+++ variable.py
@@ -0,0 +1,28 @@
+print("bhavesh poojari")
+
+print("Bhavesh welcome to the class")
+
+print("bhavesh is lkearning the python")
+
+#2 table
+a=2
+b=4
+c=6
+d=8
+e=10
+f=12
+g=14
+h=16
+i=18
+j=20
+print('welcome to the table of 2')
+print('''2x1=''',a)
+print("2x2=",b)
+print("2x3=",c)
+print("2x4=",d)
+print("2x4=",e)
+print("2x5=",f)
+print("2x6=",g)
+print("2x7",h)
+print("2x8",i)
+print("2x10=",j)
