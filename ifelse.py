9b859f0e7ba7ab37f550fe470b5a67e291342aa5
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2025-12-04 20:46:46 +0530

    Add files via upload
    4 dec 25

---
+++ ifelse.py
@@ -0,0 +1,17 @@
+
+age = 19
+
+if age > 18 :
+    print("Eligible to vote")
+else:
+    print("Not Eligible to vote")
+
+num1 = 10 
+num2 = 10
+
+if num1 > num2:
+    print("Num1 is greater")
+elif num2 > num1:
+    print("Num2 is greater")
+else:
+    print("Both are Equal")
