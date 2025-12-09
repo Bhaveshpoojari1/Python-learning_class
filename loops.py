27ac5c5f0a3be52a8c625ed1482767b6670f8f55
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2025-12-09 19:58:34 +0530

    Add files via upload
    loops concept 9 dec 25.

---
+++ loops.py
@@ -0,0 +1,23 @@
+# loops
+
+# for loop
+
+for i in range(0,10):
+    if i == 5:
+        pass
+    print(i)
+
+print('---------')
+
+for i in reversed(range(10)):
+    print(i)
+
+# while loops
+print("using the while loop")
+name = input("Enter Name : ")
+
+while name != "prasad":
+    name = input("Enter Name :")
+
+for i in range(10):
+    print(i * 2)
