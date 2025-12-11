7410c52acb5df128a1de277b96f2878c6c85cbe0
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2025-12-11 20:08:23 +0530

    continuos_table_running
    ading additional code to a continuous table

--- table_design.py
+++ table_design.py
@@ -15,4 +15,40 @@ print("Access grant!")
 number = int(input("enter which table you wanted : "))
 for i in range(0,10):
     print(i*number)
+
 print("the table of",number,"IS:")
+
+
+
+#continuos table code design without stop a code run 
+code:
+
+print("wlc Bhavesh Poojari")
+print("Welcome to the world of table")
+print("")
+print("")
+
+pass_word = "Bhavesh@123"
+
+while True:
+    pass_input = str(input("Pls enter the password:"))
+    
+    if pass_input == pass_word:
+        print("Access Granted")
+        break #exit the password
+    else:
+        print("Access Denied Wrong Password")
+        
+while True:
+    print("-" * 30) #visual separator for new request 
+    print("Wlc to the world of the table")
+    try:
+        table = int(input("pls enter the the table you want to display(Or type exit to quit:"))
+        
+        
+        print(f"the table of {table}--")
+        
+        for i in range (0,11):
+            print(table,"*",i,"=",i*table)
+    except ValueError:
+        print("Invalid Input pls enter the valid integer of the table number.")
