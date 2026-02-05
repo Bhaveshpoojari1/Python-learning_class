6f66986405811db59aaf4219fb0632d3f58799e2
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2026-02-05 20:40:12 +0530

    the changes for understanding th for loop concept
    Reformatted and clarified comments in dictionary.py.

--- dictionary.py
+++ dictionary.py
@@ -22,5 +22,9 @@ print(person)
 person['age'] = 36
 print(person)
 
+# This code is going to be used for a arrange a value in the form of a horixzontal to vertical value.
+
+# for loop going to bre used 
 for i,val in person.items():
     print(i,":",val)
+
