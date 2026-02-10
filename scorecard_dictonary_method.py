5f8416d566c6c297fb022240d475bf60d534c99f
Bhavesh Poojary <133777069+Bhaveshpoojari1@users.noreply.github.com>
2026-02-10 23:06:36 +0800

    Add files via upload

---
+++ scorecard_dictonary_method.py
@@ -0,0 +1,60 @@
+students = [
+    {
+        "person_id" : 1,
+        "name" : "prasad",
+        "age" : 21,
+        "scoreCard" : {
+            "subject1" : 21,
+            "subject2" : 35,
+            "subject3" : 53
+        }
+    },
+    {
+        "person_id" : 2,
+        "name" : "prasad",
+        "age" : 21,
+        "scoreCard" : {
+            "subject1" : 21,
+            "subject2" : 35,
+            "subject3" : 53
+        }
+    },
+    {
+        "person_id" : 3,
+        "name" : "prasad",
+        "age" : 21,
+        "scoreCard" : {
+            "subject1" : 21,
+            "subject2" : 35,
+            "subject3" : 53
+        }
+    },
+    {
+        "person_id" :4,
+        "name" : "prasad",
+        "age" : 21,
+        "scoreCard" : {
+            "subject1" : 21,
+            "subject2" : 35,
+            "subject3" : 53
+        }
+    },
+    {
+        "person_id" : 5,
+        "name" : "prasad",
+        "age" : 21,
+        "scoreCard" : {
+            "subject1" : 21,
+            "subject2" : 89,
+            "subject3" : 53
+        }
+
+    }
+]
+
+
+print("-----")
+print("----")
+print("Printing the score card into a vertical form ")
+for i in students[4]['scoreCard'].items():
+    print(i)
